836fd9e1929bf30f61218838f164e7339af192ba
Vishal Chaturvedi <vishal.chaturvedi@encoresky.com>
2022-12-28 12:54:22 +0530

    Updates

---
+++ precache-manifest.c94f5731a9cde9a2c13080f039d89f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e292da27057553f51d581fa5ca1087a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dbfbf0868a8a9d079ec5",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "a85dbf260b61fde66cae",
+    "url": "./static/js/2.2f6b0306.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2f6b0306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfbf0868a8a9d079ec5",
+    "url": "./static/js/main.3b63832f.chunk.js"
+  },
+  {
+    "revision": "38139a3d9e1f9085e747",
+    "url": "./static/js/runtime-main.7f89dca8.js"
+  }
+]);
